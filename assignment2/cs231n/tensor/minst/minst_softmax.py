1234bceda0631b5de050558b20a03069d5db21c7
游朝江 <cjyou20110920sspu@163.com>
2016-11-06 19:46:01 +0800

    Update minst_softmax.py
    add notations

--- assignment2/cs231n/tensor/minst/minst_softmax.py
+++ assignment2/cs231n/tensor/minst/minst_softmax.py
@@ -101,35 +101,41 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
-
+# first convolution layer
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
+# change input to the format we need [batch heigth weight channel]
 x_image = tf.reshape(x, [-1, 28, 28, 1])
-
+# layer inside calculation include convolution relu pooling
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
+# seconde convolution layer
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
+# the first fully connected layer (note: except the last fc layer, they all need to connect with relu)
 W_fc1 = weight_variable([7 * 7 * 64, 1024])
 b_fc1 = bias_variable([1024])
 
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
+# the second fc layer 
 W_fc2 = weight_variable([1024, 84])
 b_fc2 = bias_variable([84])
 
 h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
 
+# handle with dropout(随机失活)
 keep_prob = tf.placeholder(tf.float32)
 h_fc2_drop = tf.nn.dropout(h_fc2, keep_prob)
 
+# the last fc layer without relu
 W_fc3 = weight_variable([84, 10])
 b_fc3 = bias_variable([10])
 
@@ -138,11 +144,16 @@ y_conv = tf.matmul(h_fc2_drop, W_fc3) + b_fc3
 ########################################################################################################################
 # define cal function for this model: the predict val is stored in y_conv,the correct label is stored in y_, all you need
 # to do is to set the feed_dict
+
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, y_))
+# define train function
 train_step = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+# define predict function
 predict_function = tf.argmax(y_conv, 1)
+# define accuracy function
 correct_prediction = tf.equal(predict_function, tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))  # Casts bool to a new type tf.float32
+
 # define all is over!!
 # start to run
 
