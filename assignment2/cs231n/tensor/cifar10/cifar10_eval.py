fda259647ab62b84c190461b99eed55c1d5fe029
shanghai-Jerry <cjyou20110920sspu@163.com>
2016-11-23 13:22:19 +0800

    no ideas

--- assignment2/cs231n/tensor/cifar10/cifar10_eval.py
+++ assignment2/cs231n/tensor/cifar10/cifar10_eval.py
@@ -46,7 +46,7 @@ tf.app.flags.DEFINE_string('eval_dir', 'cifar10_eval',
                            """Directory where to write event logs.""")
 tf.app.flags.DEFINE_string('eval_data', 'test',
                            """Either 'test' or 'train_eval'.""")
-tf.app.flags.DEFINE_string('checkpoint_dir', '/mnt/hgfs/cs231n/cs231n/assignment2/cs231n/tensor/cifar10/cifar10_train',
+tf.app.flags.DEFINE_string('checkpoint_dir', 'cifar10_train',
                            """Directory where to read model checkpoints.""")
 tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
                             """How often to run the eval.""")
